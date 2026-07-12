b30fc1e28bcfa373eb9007bd913d5d7f5edc9ef8
김남욱 <wsh6922@naver.com>
2026-07-12 19:09:57 +0900

    [feat]: CI workflow 추가 + 단위/통합 테스트 분리

--- src/test/java/com/ktb/KtbApplicationTests.java
+++ src/test/java/com/ktb/KtbApplicationTests.java
@@ -1,9 +1,11 @@
 package com.ktb;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+@Tag("integration")
 @ActiveProfiles("test")
 @SpringBootTest
 class KtbApplicationTests {
